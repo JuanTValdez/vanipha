89c1d611128425cc2f65d2a2dc11b9c737ba7ca6
Juan Valdez <juantvaldez85@gmail.com>
2024-01-29 17:46:19 -0500

    save

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<h1>New Site Coming Soon</h1>
-			<h2>Spring 2024</h2>
-			<h4>Insert Contact Info and Address</h4>
+			<div className='temp-style'>
+				<h1>New Site Coming Soon</h1>
+				<h2>Spring 2024</h2>
+				<h4>Insert Contact Info and Address</h4>
+			</div>
 		</div>
 	);
 }
