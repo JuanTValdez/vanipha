20c8e93126cd686c0512ef43756aa094123c07b3
Juan Valdez <juantvaldez85@gmail.com>
2024-02-06 17:44:54 -0500

    added boiler plate

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ function App() {
 	return (
 		<div className='App'>
 			<div className='grid-col'>
-				<h1>New Site Coming Soon</h1>
-				<h2>Spring 2024</h2>
-				<h4>Insert Contact Info and Address</h4>
+				<div className='left-col'>
+					<h1>Coming Soon</h1>
+					<h2>Spring 2024</h2>
+					<h4>
+						We're cooking up something great and can't wait to share our new
+						website – stay tuned!
+					</h4>
+					<div className='contact-info'>
+						<div className='address'>
+							<h6>Kitchen</h6>
+							<p>221 Geary Ave.</p>
+							<p>Toronto, ON</p>
+							<p>M6H 2C1</p>
+						</div>
+						<div>
+							<h6>Contact</h6>
+							<p>(416)-484-9625</p>
+							<p>info@vanipha.com</p>
+						</div>
+					</div>
+				</div>
+				<div className='right-col'>
+					<img src='' alt='' className='abs' />
+				</div>
 			</div>
 		</div>
 	);
