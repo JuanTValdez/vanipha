558fe2800525231f0afef8cc7bc055c1e243f4b0
Juan Valdez <juantvaldez85@gmail.com>
2024-02-07 17:26:45 -0500

    save

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import Vanipha from './images/vanipha.png';
 function App() {
 	return (
 		<div className='App'>
-			{/* <img className='logo' src={Vanipha} alt='logo' /> */}
+			<img className='logo' src={Vanipha} alt='logo' />
 			<div className='grid-col'>
 				<div className='left-col'>
 					<h1 >Coming Soon</h1>
 					<h2>Spring 2024</h2>
-					<h4>
+					<h3>
 						We're cooking up something great and can't wait to share our new
 						website – stay tuned!
-					</h4>
+					</h3>
 					<div className='contact-info'>
 						<div className='address'>
-							<h6>Kitchen</h6>
+							<h4>Kitchen</h4>
 							<p>221 Geary Ave.</p>
 							<p>Toronto, ON</p>
 							<p>M6H 2C1</p>
 						</div>
 						<div>
-							<h6>Contact</h6>
+							<h4>Contact</h4>
 							<p>(416)-484-9625</p>
 							<p>info@vanipha.com</p>
 						</div>
