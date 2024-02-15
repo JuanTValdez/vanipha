e22997e46feade81f919c086dbd4a6f7ad2eb23c
Juan Valdez <juantvaldez85@gmail.com>
2024-02-15 17:29:39 -0500

    start desktop layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Vanipha2 from './images/vanipha_logo_x2.png';
 function App() {
 	return (
 		<div className='App'>
+			<div className='logo'>
+				<img src={Vanipha2} alt='logo' />
+			</div>
 			<div className='grid-col'>
-				<div className='logo'>
-					<img src={Vanipha2} alt='logo' />
-				</div>
 				<div className='left-col'>
 					<h1 className='coming-soon inter-tight-font'>Coming Soon</h1>
 					<h6 className='spring-2024 inter-font'>Spring 2024</h6>
@@ -17,13 +17,13 @@ function App() {
 						website – stay tuned!
 					</p>
 					<div className='contact-info'>
-						<div >
+						<div>
 							<h4 className='caption-caps inter-font'>Kitchen</h4>
 							<p className='caption'>221 Geary Ave.</p>
 							<p className='caption'>Toronto, ON</p>
 							<p className='caption'>M6H 2C1</p>
 						</div>
-						<div> 
+						<div>
 							<h4 className='caption-caps inter-font'>Contact</h4>
 							<p className='caption'>(416)-484-9625</p>
 							<p className='caption'>info@vanipha.com</p>
