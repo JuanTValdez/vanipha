476923685b38937b0bdfd3bb9a7e48ab67d54ae1
Juan Valdez <juantvaldez85@gmail.com>
2025-02-21 20:30:13 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Roll from './images/roll.png';
-import Vanipha2 from './images/vanipha_logo_x2.png';
+import "./App.css";
+import Roll from "./images/roll.png";
+import Vanipha2 from "./images/vanipha_logo_x2.png";
 function App() {
 	return (
 		<div className='App'>
@@ -9,9 +9,16 @@ function App() {
 			</div>
 			<div className='flex-col'>
 				<div className='left-col'>
-					<h1 className='coming-soon inter-tight-font'>Coming Soon</h1>
-					<h6 className='spring-2024 inter-font'>Summer 2024</h6>
+					<h1 className='coming-soon inter-tight-font'>
+						Coming Soon
+					</h1>
+					<h6 className='spring-2024 inter-font'>Summer 2025</h6>
 
+					<p className='description inter-font'>
+						We are excited to announce that we are currently working
+						on a new restaurant concept. Stay tuned for more
+						information.
+					</p>
 					<div className='contact-info'>
 						<div className='address'>
 							<h4 className='caption-caps inter-font'>Kitchen</h4>
@@ -22,7 +29,10 @@ function App() {
 						<div className='address'>
 							<h4 className='caption-caps inter-font'>Contact</h4>
 							<p className='caption inter-font'>(416) 484-9625</p>
-							<a href='mailto:info@vanipha.com' className='caption inter-font'>
+							<a
+								href='mailto:info@vanipha.com'
+								className='caption inter-font'
+							>
 								info@vanipha.com
 							</a>
 						</div>
